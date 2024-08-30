afabe98edd9958dbcf230001ac200993b02146d1
hieuvn2004 <22010502@st.phenikaa-uni.edu.vn>
2024-08-30 08:43:15 +0000

    code 1

---
+++ app/Book.java
@@ -0,0 +1,12 @@
+package app;
+public class Book {
+ String title;
+ String author;
+ int numPages;
+ Book() { } ; 
+ public Book(String t, String a, int p) {
+ title = t;
+ author = a;
+ numPages = p;
+ }
+}

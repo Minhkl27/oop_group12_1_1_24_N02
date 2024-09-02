17773df5082f4c4789a570855bb8ca8cd04d4413
Minhkl27 <118721048+Minhkl27@users.noreply.github.com>
2024-09-02 08:57:55 +0000

    AccessControlExercises3.java

---
+++ Week2/Access/DebugTest.java
@@ -0,0 +1,10 @@
+package Week2.Access;
+
+import static Week2.Packages.debug.*;
+
+public class DebugTest {
+    public static void main(String[] args) {
+        debugMethod("Nguyen Van Minh");
+        debugoffMethod("None");
+    }
+}
---
+++ Week2/Packages/debug.java
@@ -0,0 +1,10 @@
+package Week2.Packages;
+
+public class debug{
+    public static void debugMethod(String x) {
+        System.out.println(x);
+    }
+    public static void debugoffMethod(String y){
+        System.out.println("error");
+    }
+}

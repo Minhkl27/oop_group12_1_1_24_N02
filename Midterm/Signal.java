d0356c07c8d9866e12bd6d0eb7e1487001c8854d
Minhkl27 <118721048+Minhkl27@users.noreply.github.com>
2024-09-06 08:27:14 +0000

    Bài1Midterm

---
+++ Midterm/Signal.java
@@ -0,0 +1,22 @@
+
+
+package Midterm;
+
+interface Signal {
+    // Getter và setter cho biên độ
+    double getAmplitude();
+    void setAmplitude(double amplitude);
+
+    // Getter và setter cho tính thường xuyên
+    double getFrequency();
+    void setFrequency(double frequency);
+
+    // Getter và setter cho chu kỳ
+    double getPeriod();
+    void setPeriod(double period);
+
+    // Getter và setter cho bước sóng
+    double getWavelength();
+    void setWavelength(double wavelength);
+    
+} 

f185505cef5bc7a0a5c4bc1ac59510bdca53b870
hieuvn2004 <22010502@st.phenikaa-uni.edu.vn>
2024-09-11 05:11:12 +0000

    commit

---
+++ cau2/Radar.java
@@ -0,0 +1,13 @@
+package cau2;
+import java.util.List;
+import java.util.ArrayList;
+class Radar {
+    public List<Double> analyzeSignal(Signal signal, int n) {
+        List<Double> analyzedSignal = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            double result = 1 - (double) i / 15;
+            analyzedSignal.add(result);
+        }
+        return analyzedSignal;
+    }
+}

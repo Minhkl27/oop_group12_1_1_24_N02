4eaae1669047b134d9aac1e5f83fbbb6d639bddf
Minhkl27 <118721048+Minhkl27@users.noreply.github.com>
2024-09-11 07:19:03 +0000

    Cấu trúc lại source code bài giữa kì

---
+++ Midterm/demo/src/main/java/com/example/Excercise1/Main.java
@@ -0,0 +1,12 @@
+package com.example.Excercise1;
+
+public class Main {
+    public static void main(String[] args) {
+        DiscreteSignal signal = new DiscreteSignal();
+        signal.setAmplitude(5.0);
+        signal.setFrequency(1000.0);
+        signal.setPeriod(0.001);
+        signal.setWavelength(0.3);
+        signal.displaySignalInfo();
+    }
+}

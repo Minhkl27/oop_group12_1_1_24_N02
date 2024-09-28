f61a32632f1023c94e4989c5fbfc39f651b75b78
MinhPC1337 <you@example.com>
2024-09-28 13:31:57 +0700

    try-cat for loginform

--- Manage_Students/demo/src/main/java/com/example/SignInController.java
+++ Manage_Students/demo/src/main/java/com/example/SignInController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -20,20 +21,71 @@ public class SignInController {
     TextField nameTextField;
 
     @FXML
-    public void btnSignInOnClicked(ActionEvent event) throws IOException {
+    TextField password;
 
-        String username = nameTextField.getText();
+    private final String ACCOUNTadmin = "MinhNguyen";
+    private final String PASSWORDadmin = "minh12345";
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Homepage.fxml"));
-        root = loader.load();
+    private String userACCOUNT = "HieuVn";
+    private String userPASSWORD = "hieu12345";
 
-        Homepage homepage = loader.getController();
-        homepage.displayname(username);
-        // root = FXMLLoader.load(getClass().getResource("Homepage.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    private boolean handleLogin() {
+        if (nameTextField.getText().equals(ACCOUNTadmin) && password.getText().equals(PASSWORDadmin)) {
+            // Đăng nhập thành công
+            showAlert(Alert.AlertType.INFORMATION, "Đăng Nhập Thành Công", "Admin access.");
+            return true;
+        } else if (nameTextField.getText().equals(userACCOUNT) && password.getText().equals(userPASSWORD)) {
+            // Đăng nhập thành công
+            showAlert(Alert.AlertType.INFORMATION, "Đăng Nhập Thành Công", "Bạn đã đăng nhập thành công.");
+            return true;
+        } else {
+            // Đăng nhập thất bại
+            showAlert(Alert.AlertType.ERROR, "Đăng Nhập Thất Bại", "Tên đăng nhập hoặc mật khẩu không chính xác.");
+            return false;
+        }
     }
 
+    private void showAlert(Alert.AlertType alertType, String title, String content) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    @FXML
+    public void btnSignInOnClicked(ActionEvent event) throws IOException {
+        try {
+            if (handleLogin()) {
+                String username = nameTextField.getText();
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Homepage.fxml"));
+                root = loader.load();
+
+                Homepage homepage = loader.getController();
+                homepage.displayname(username);
+                // root = FXMLLoader.load(getClass().getResource("Homepage.fxml"));
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // finally {
+        // String username = nameTextField.getText();
+
+        // FXMLLoader loader = new FXMLLoader(getClass().getResource("Homepage.fxml"));
+        // root = loader.load();
+
+        // Homepage homepage = loader.getController();
+        // homepage.displayname(username);
+        // // root = FXMLLoader.load(getClass().getResource("Homepage.fxml"));
+        // stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        // scene = new Scene(root);
+        // stage.setScene(scene);
+        // stage.show();
+        // }
+
+    }
 }
